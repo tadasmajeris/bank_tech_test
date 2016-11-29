9c471f2bb825ce63753f03664e61e256c8f08010
Tadas Majeris <tadas.majeris@gmail.com>
2016-11-29 13:01:48 +0000

    deposit method test pass

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,4 +6,12 @@ describe Account do
     expect(subject.balance).to eq 0
   end
 
+  describe '#deposit' do
+    it 'allows to deposit money into account' do
+      subject.deposit(500)
+      subject.deposit(200)
+      expect(subject.balance).to eq 700
+    end
+  end
+
 end
